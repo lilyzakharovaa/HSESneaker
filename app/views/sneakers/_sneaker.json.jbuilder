4da90f7737d6f3dc4a630a64bb90fa5863985adc
lilyzakharovaa <lika03480@gmail.com>
2017-05-25 00:20:23 +0300

    load images

---
+++ app/views/sneakers/_sneaker.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! sneaker, :id, :brand_id, :model, :price, :created_at, :updated_at
+json.url sneaker_url(sneaker, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524153924) do
+ActiveRecord::Schema.define(version: 20170524202738) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
     t.string   "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sneakers", force: :cascade do |t|
+    t.integer  "brand_id"
+    t.string   "model"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
