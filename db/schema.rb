57865637d912ed05a6fadc158ae6fd06a9550daa
lilyzakharovaa <lika03480@gmail.com>
2017-06-14 02:23:00 +0300

    almost done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524202738) do
+ActiveRecord::Schema.define(version: 20170613195242) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170524202738) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+    t.string   "sex"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Rake::Task['db:drop'].invoke
+Rake::Task['db:create'].invoke
+Rake::Task['db:migrate'].invoke
+
+def random_brand_id
+  Brand.offset(rand(Brand.count)).first.id
+  # brands_quantity = Brand.count
+  # brands_random_offset = rand(brands_quantity)
+  # random_brand = Brand.offset(brands_random_offset).first
+  # random_brand_id = random_brand_id
+  # random_brand_id
+end
+
+def upload_fake_image
+  uploader = ImageUploader.new(Sneaker.new, :image)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'lib/tasks/sneakers', '*')).sample))
+  uploader
+end
+
 Brand.create([
   {
     name: 'NIKE',
@@ -28,5 +47,28 @@ Brand.create([
   } , {
     name: 'Jordan',
     logo: ''
-  }   
+  }
 ])
+
+@sneaker_models = ['huarache', 'air', 'superstar']
+
+@sneaker_sexes = ['men', 'women']
+
+def artwork_price
+  rand(3600..9900)
+end
+
+def create_sneaker
+  Sneaker.create(
+    brand_id: random_brand_id,
+    model:    @sneaker_models.sample,
+    sex:      @sneaker_sexes.sample,
+    price:    artwork_price,
+    image:    upload_fake_image
+  )
+end
+
+100.times do
+  sneaker = create_sneaker
+  puts "Sneaker #{sneaker.id} created"
+end
